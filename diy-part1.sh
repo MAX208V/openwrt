35886d14c78a8ad1347c57cc5a2f8600893688e7
MAX208V <48180524+MAX208V@users.noreply.github.com>
2024-04-16 23:07:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,11 +8,11 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # git获取源码
 git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package package/sirpdboy
+
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
